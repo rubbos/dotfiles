a0af8d2930e90b484517acc7ff722abc57e34e17
rubbos <41753645+rubbos@users.noreply.github.com>
2025-10-06 21:51:33 +0200

    added some plugins and mason to nvim

---
+++ nvim/init.lua
@@ -0,0 +1,25 @@
+vim.o.number = true
+vim.o.relativenumber = false
+vim.o.wrap = false
+vim.o.tabstop = 4
+vim.o.swapfile = false
+
+-- keymaps
+vim.g.mapleader = " "
+vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
+vim.keymap.set('n', '<leader>w', ':write<CR>')
+--vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+
+vim.pack.add({
+	{ src = "https://github.com/nyoom-engineering/oxocarbon.nvim" },
+	{ src = "https://github.com/nvim-mini/mini.pick" },
+	{ src = "https://github.com/nvim-mini/mini.files" },
+	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
+	{ src = "https://github.com/mason-org/mason.nvim" },
+})
+
+require "mason".setup()
+require "mini.pick".setup()
+require "mini.files".setup()
+
+vim.cmd("colorscheme oxocarbon")
