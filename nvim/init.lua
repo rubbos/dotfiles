1127551cd624ba25ed5cb91831d76ed63568c438
rubbos <41753645+rubbos@users.noreply.github.com>
2025-10-12 23:01:08 +0200

    changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,6 @@
 vim.o.number = true
 vim.o.relativenumber = false
-vim.o.wrap = false
+vim.o.wrap = true
 vim.o.tabstop = 4
 vim.o.swapfile = false
 
@@ -12,14 +12,17 @@ vim.keymap.set('n', '<leader>w', ':write<CR>')
 
 vim.pack.add({
 	{ src = "https://github.com/nyoom-engineering/oxocarbon.nvim" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 	{ src = "https://github.com/nvim-mini/mini.pick" },
 	{ src = "https://github.com/nvim-mini/mini.files" },
 	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
 	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" },
 })
 
 require "mason".setup()
 require "mini.pick".setup()
 require "mini.files".setup()
+require "render-markdown".setup()
 
 vim.cmd("colorscheme oxocarbon")
