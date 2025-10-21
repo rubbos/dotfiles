6d7fcbbce75124e5803bf3e0e199079056952173
rubbos <41753645+rubbos@users.noreply.github.com>
2025-10-21 14:04:35 +0200

    changes + added plugins to nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,9 @@
 vim.o.number = true
 vim.o.relativenumber = false
-vim.o.wrap = true
-vim.o.tabstop = 4
+vim.o.wrap = false
+vim.o.tabstop = 2
 vim.o.swapfile = false
+vim.o.scrolloff = 10
 
 -- keymaps
 vim.g.mapleader = " "
@@ -11,16 +12,39 @@ vim.keymap.set('n', '<leader>w', ':write<CR>')
 --vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
 
 vim.pack.add({
-	{ src = "https://github.com/nyoom-engineering/oxocarbon.nvim" },
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
-	{ src = "https://github.com/nvim-mini/mini.pick" },
-	{ src = "https://github.com/nvim-mini/mini.files" },
-	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
-	{ src = "https://github.com/mason-org/mason.nvim" },
-	{ src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" },
+				{ src = "https://github.com/nyoom-engineering/oxocarbon.nvim" }, 
+				-- language parser and better syntax highlighting
+				{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+				{ src = "https://github.com/nvim-mini/mini.pick" },
+				{ src = "https://github.com/nvim-mini/mini.files" },
+				{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
+				{ src = "https://github.com/mason-org/mason.nvim" },
+				{ src = "https://github.com/akinsho/toggleterm.nvim" },
+				{ src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" },
 })
 
 require "mason".setup()
+
+local languages = {"python", "java", "rust", "c", "lua", "vim", "vimdoc", "markdown" }
+require "nvim-treesitter".install(languages)
+require "nvim-treesitter".setup({
+				highlight = { enable = true },
+				indent = { enable = true }
+})
+vim.api.nvim_create_autocmd('FileType', {
+				pattern = languages,
+				callback = function()
+								vim.treesitter.start()
+								-- enable indentation on Tree-sitter
+								vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end,
+})
+
+require "toggleterm".setup({
+				open_mapping = "<C-t>",
+				direction = "float",
+}) 
+
 require "mini.pick".setup()
 require "mini.files".setup()
 require "render-markdown".setup()
